b90e4dfc975326043346f11da7c9888ed3d31c3e
Simon Sedlatschek <code@sedlatschek.com>
2025-03-05 20:57:43 +0100

    improve popup record button

--- src/popup/PopupPage.vue
+++ src/popup/PopupPage.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="w-96 p-2">
-    <BtnIcon
-      title="Start recording"
-      @click="start">
-      <IconStart />
-    </BtnIcon>
-    <div>
+    <div class="flex w-full justify-center">
+      <BtnIcon
+        class="w-38"
+        :title="buttonTitle"
+        @click="start">
+        <IconCaretLeft v-if="currentRecordings.length > 0" />
+        <IconStart v-else />
+        <span class="ms-2">{{ buttonTitle }}</span>
+      </BtnIcon>
+    </div>
+
+    <div
+      v-if="currentRecordings.length > 0"
+      class="mt-4">
       <RecordingWidget
         v-for="recording in currentRecordings"
         :key="recording.id"
@@ -23,6 +31,7 @@ import { createRecordingsRef } from '../common/recordingsRef';
 import RecordingWidget from '../common/RecordingWidget.vue';
 import { getCurrentTabId } from '../common/tabs';
 import BtnIcon from '../components/BtnIcon.vue';
+import IconCaretLeft from '../components/IconCaretLeft.vue';
 import IconStart from '../components/IconStart.vue';
 
 const messageBus = new MessageBus('Popup');
@@ -38,5 +47,8 @@ const recordings = createRecordingsRef(messageBus, true);
 
 const currentRecordings = computed(() =>
   recordings.value.filter((r) => r.tabId === tabId.value && !r.stoppedAtTs),
+);
+const buttonTitle = computed(() =>
+  currentRecordings.value.length > 0 ? 'Restart recording' : 'Start recording',
 );
 </script>
